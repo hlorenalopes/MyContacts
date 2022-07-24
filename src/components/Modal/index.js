6ad9076af9f869cfce454bf7afef06cd97aa851a
hlorenalopes <hlorenalopes@gmail.com>
2022-07-23 19:04:19 -0600

    add Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,20 @@
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export default function Modal() {
+  return (
+    <Overlay>
+      <Container>
+        <h1>Modal title</h1>
+        <p>Modal body</p>
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancel
+          </button>
+          <Button type="button">Delete</Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
