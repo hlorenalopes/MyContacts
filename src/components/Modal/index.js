fc442dc063d040b11785a2fbc3a9d287b1dcb4c2
hlorenalopes <hlorenalopes@gmail.com>
2022-07-23 19:30:15 -0600

    add Modal danger status

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,32 @@
+import PropTypes from 'prop-types';
+
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Modal title</h1>
         <p>Modal body</p>
         <Footer>
           <button type="button" className="cancel-button">
             Cancel
           </button>
-          <Button type="button">Delete</Button>
+          <Button danger={danger} type="button">
+            Delete
+          </Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
