f758d1c8c510a53accc818044c77597c48c086f4
hlorenalopes <hlorenalopes@gmail.com>
2022-08-31 11:20:24 -0600

    body parse based on Content-Type

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -10,11 +10,27 @@ class HttpClient {
 
     const response = await fetch(`${this.baseURL}${path}`);
 
-    if (response.ok) {
-      return response.json();
+    let body = null;
+
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    // verbose
+    // const errorMessage = body
+    //   ? body.error
+    //   : `${response.status} - ${response.statusText}`;
+    // throw new Error(errorMessage);
+
+    // simpler - optional chaining
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`
+    );
   }
 }
 
