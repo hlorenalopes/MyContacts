e67e93f2e1f6c640e3a39f14765955e908b443c9
hlorenalopes <hlorenalopes@gmail.com>
2022-09-11 10:10:10 -0600

    add contact creation

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,12 +1,33 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log(response);
+    } catch {
+      alert('An error ocurred while trying to add the contact.');
+    }
+  }
+
   return (
     <>
       <PageHeader title="New Contact" />
 
-      <ContactForm buttonLabel="Register" />
+      <ContactForm
+        buttonLabel="Add"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
