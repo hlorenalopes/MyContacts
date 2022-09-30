cdfb4345f1d9a1d3582f557d858f97c157518b29
hlorenalopes <hlorenalopes@gmail.com>
2022-09-30 15:12:58 -0600

    add loading contacts in edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,51 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+
+        console.log({ contactData });
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contact not found.'
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Edit Mateus Silva" />
 
-      <ContactForm buttonLabel="Save" />
+      <ContactForm buttonLabel="Save" onSubmit={handleSubmit} />
     </>
   );
 }
