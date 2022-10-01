20afd37fa5658a82178deb390d4e67132dda24c9
hlorenalopes <hlorenalopes@gmail.com>
2022-10-01 16:17:21 -0600

    add edit contact implementation

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -88,10 +88,6 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     });
 
     setIsSubmitting(false);
-    setName('');
-    setEmail('');
-    setPhone('');
-    setCategoryId('');
   }
 
   return (
